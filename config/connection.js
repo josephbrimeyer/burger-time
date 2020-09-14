45d905b9c0f7c200d2ebf8ce2bf9fd337c0e5edc
Joe Brimeyer <jbrimeyer@gmail.com>
2020-09-14 13:45:26 -0700

    created schema and seeds.

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "#Nts44cqa@",
-  database: "burger-time_db",
+  database: "burgertime_db",
 });
 
 // Make connection.
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Bacon Cheeseburger, false');
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Plain Burger', false);
+INSERT INTO burgers
+    (name, sleepy)
+VALUES
+    ('Vegi Burger', false);
