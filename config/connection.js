1eb8fd05f64f78c677872a0b489077415a78d672
Joe Brimeyer <jbrimeyer@gmail.com>
2020-09-22 10:18:18 -0700

    deploy to heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "#Nts44cqa@",
-  database: "burgertime_db",
-});
+// For Heroku deployment and jawsdb.
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "w1h4cr5sb73o944p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "cil0bxedlqwx61nj",
+    password: "ncynqfgp9w75qrjz",
+    database: "ei63m9hzhu4znu0z",
+  });
+}
 
 // Make connection.
 connection.connect(function (err) {
